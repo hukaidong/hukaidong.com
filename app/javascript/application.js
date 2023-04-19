5542a9f44c2b72a93e9cc40b1a88c6d18034001a
Kaidong Hu <hukaidonghkd@gmail.com>
2023-04-19 15:03:53 -0400

    Initial commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery3
+//= require jquery_ujs
+//= require semantic.min.js
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
+$(document)
+    .ready(function() {
+        $('.ui.menu .ui.dropdown').dropdown({
+            on: 'hover'
+        });
+        $('.ui.menu a.item')
+            .on('click', function() {
+                $(this)
+                    .addClass('active')
+                    .siblings()
+                    .removeClass('active')
+                ;
+            })
+        ;
+    })
+;
