befbb12b85ce77c902df5440a4ccd1d870be4fc5
Kaidong Hu <hukaidonghkd@gmail.com>
2023-04-22 14:06:50 -0400

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :guests
+  devise_for :users
   get 'search', to: 'home#search'
   root 'home#main'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
